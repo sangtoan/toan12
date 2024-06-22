0d54887f94e9b61e482602c659b6f82b6d0ffa46
sangtoan <116827655+sangtoan@users.noreply.github.com>
2024-06-22 16:29:24 +0700

    Update adjustPaths.js

--- adjustPaths.js
+++ adjustPaths.js
@@ -1,12 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Lấy tất cả các thẻ img
     const images = document.querySelectorAll("img");
-
     images.forEach(img => {
         let src = img.getAttribute("src");
-        // Kiểm tra nếu src bắt đầu bằng "../../khohinh"
         if (src.startsWith("../../khohinh")) {
-            // Điều chỉnh src để phù hợp với GitHub Pages
             src = src.replace("../../khohinh", "https://raw.githubusercontent.com/sangtoan/toan12/main/khohinh");
             img.setAttribute("src", src);
         }
