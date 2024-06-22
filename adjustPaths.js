6317609c0e125bdfb4e454b3cc4d8bb16d868a91
sangtoan <116827655+sangtoan@users.noreply.github.com>
2024-06-22 16:52:59 +0700

    Update adjustPaths.js

--- adjustPaths.js
+++ adjustPaths.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+    console.log("adjustPaths.js is loaded");
     const images = document.querySelectorAll("img");
     images.forEach(img => {
         let src = img.getAttribute("src");
+        console.log("Original src:", src);
         if (src.startsWith("../../khohinh")) {
             src = src.replace("../../khohinh", "https://raw.githubusercontent.com/sangtoan/toan12/main/khohinh");
+            console.log("Updated src:", src);
             img.setAttribute("src", src);
         }
     });
