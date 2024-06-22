202fa005955b63bcb79d7e884c8443c282e7108a
sangtoan <116827655+sangtoan@users.noreply.github.com>
2024-06-22 11:30:49 +0700

    Add files via upload

--- tree.js
+++ tree.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
   
-  function showContent(url) {
+  function showContentG(url) {
     var pdfViewer = document.getElementById('pdfViewer');
     var htmlContent = document.getElementById('htmlContent');
   
@@ -30,3 +30,32 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
   
+  function showContent(url) {
+    var pdfViewer = document.getElementById('pdfViewer');
+    var htmlContent = document.getElementById('htmlContent');
+
+    if (url.endsWith(".pdf")) {
+        pdfViewer.src = url;
+        pdfViewer.style.display = 'block';
+        htmlContent.style.display = 'none';
+    } else {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.text();
+            })
+            .then(data => {
+                htmlContent.innerHTML = data;
+                pdfViewer.style.display = 'none';
+                htmlContent.style.display = 'block';
+                
+                // Convert LaTeX to HTML if needed
+                renderLatexInElement(htmlContent);
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+    }
+  }
